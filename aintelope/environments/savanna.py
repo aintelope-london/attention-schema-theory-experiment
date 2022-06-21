18994e3ecd45b20a933990f2ff6a656d8cda7c45
Hauke Rehfeld <git@haukerehfeld.de>
2022-06-21 11:28:49 +0200

    cln: savannah: factor out agents

--- aintelope/environments/savanna.py
+++ aintelope/environments/savanna.py
@@ -8,6 +8,7 @@ from pettingzoo.utils import wrappers, agent_selector
 import pygame
 import random
 from pprint import pprint
+from collections import defaultdict, UserDict
 
 NUM_ITERS = 500  # duration of the game
 # TODO: MAP_SIZE or MAP_EXTENT?
@@ -269,17 +270,89 @@ def env():
     return env
 
 
+class RandomWalkAgent:
+    def __call__(self, action_space, observation, reward, info):
+        return action_space.sample()
+
+
+EPS = 0.0001
+class IterativeWeightOptimizationAgent:
+    def __init__(self):
+        self.is_initialized = False
+
+    def __call__(self, action_space, observation, reward, info):
+        MIN_WEIGHT = 0.05
+        learning_rate = 0.01
+        learning_randomness = 0.00
+
+        LAST_ACTION_KEY = 'last_action'
+        LAST_REWARD_KEY = 'last_reward'
+        ACTIONS_WEIGHTS = 'actions_weights'
+
+        if not self.is_initialized:
+            info[ACTIONS_WEIGHTS] = np.repeat([1.0], action_space.n)
+            self.is_initialized = True
+
+        print('step:', reward, observation)
+        last_action = info.get(LAST_ACTION_KEY)
+        last_reward = info.get(LAST_REWARD_KEY, 0)
+        action_weights = info[ACTIONS_WEIGHTS]
+        # avoid big weight change on the first valid step
+        if last_action is not None and last_reward > EPS:
+            last_action_reward_delta = reward - last_reward
+            last_action_weight = action_weights[last_action]
+            print('dreward',last_action_reward_delta, last_action, ACTION_MAP[last_action])
+            last_action_weight += last_action_reward_delta * learning_rate
+            last_action_weight = max(MIN_WEIGHT, last_action_weight)
+            action_weights[last_action] = last_action_weight
+            print('action_weights', action_weights)
+
+            weight_sum = np.sum(action_weights)
+            action_weights /= weight_sum
+
+        def cdf(ds):
+            res = {}
+            x = 0
+            for k, v in ds:
+                x += v
+                res[k] = x
+            for k in res:
+                res[k] /= x
+            return res
+
+        def choose(cdf):
+            assert cdf
+            x = random.uniform(0, 1-EPS)
+            k = None
+            for k, v in cdf.items():
+                if x >= v:
+                    return k
+            return k
+
+        action_weights_cdf = cdf(enumerate(action_weights))
+        print('cdf', ', '.join([f'{ACTION_MAP[iaction]}: {w}' for iaction, w in action_weights_cdf.items()]))
+
+        pprint(action_weights_cdf)
+        action = choose(action_weights_cdf)
+        if random.uniform(0, 1) < learning_randomness:
+            action = action_space.sample()
+        info[LAST_ACTION_KEY] = action
+        info[LAST_REWARD_KEY] = reward
+        print('chose action', action, ACTION_MAP[action])
+        return action
+
+
 def main(env: RawEnv):
+    policy = IterativeWeightOptimizationAgent()
     env.reset()
     for agent in env.agent_iter():
         observation, reward, done, info = env.last()
 
+
         action_space:Discrete = env.action_space(agent)
-
-
-        # action = policy(observation, agent)
         if not done:
-            action = action_space.sample()
+            action = policy(action_space, observation, reward, info)
+            assert action in action_space
         else:
             action = None
         env.step(action)
