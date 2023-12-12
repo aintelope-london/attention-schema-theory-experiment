c4a52b704a4b3cdf9ac646f960da393398dfb4b6
Roland Pihlakas <roland@simplify.ee>
2023-12-12 09:22:31 +0200

    Ensure that tests are run only once in Git automatic tests.

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,7 +1,10 @@
 import pytest
 import sys
+import os
 from aintelope.config.config_utils import register_resolvers
 
-if __name__ == "__main__" and sys.gettrace() is not None:  # detect debugging
+if (
+    __name__ == "__main__" and os.name == "nt" and sys.gettrace() is not None
+):  # detect debugging
     register_resolvers()  # needs to be called only once, needed for test_trainer.py
     pytest.main(["tests/"])  # run tests only in this folder and its subfolders
