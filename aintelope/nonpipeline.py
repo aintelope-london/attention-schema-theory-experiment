ef4e55cf181e03f0f9a6a77d0f5a73c605307d95
Roland Pihlakas <roland@simplify.ee>
2025-01-01 12:29:47 +0200

    Fixing experiment_name in nonpipeline.py

--- aintelope/nonpipeline.py
+++ aintelope/nonpipeline.py
@@ -35,7 +35,7 @@ def aintelope_main(cfg: DictConfig) -> None:
     # train
     num_actual_train_episodes = run_experiment(
         cfg,
-        experiment_name="Nonpipeline",
+        experiment_name=cfg.experiment_name,
         score_dimensions=score_dimensions,
         test_mode=False,
         i_pipeline_cycle=0,
@@ -44,20 +44,20 @@ def aintelope_main(cfg: DictConfig) -> None:
     # test
     run_experiment(
         cfg,
-        experiment_name="Nonpipeline",
+        experiment_name=cfg.experiment_name,
         score_dimensions=score_dimensions,
         test_mode=True,
         i_pipeline_cycle=0,
         num_actual_train_episodes=num_actual_train_episodes,
     )
 
-    title = timestamp + " : Nonpipeline"
+    title = timestamp + " : " + cfg.experiment_name
     do_not_show_plot = cfg.hparams.unit_test_mode
     analytics(
         cfg,
         score_dimensions,
         title=title,
-        experiment_name="Nonpipeline",
+        experiment_name=cfg.experiment_name,
         do_not_show_plot=do_not_show_plot,
     )
 
