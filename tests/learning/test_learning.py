39d6cbdd3eb81090f8c454f79e8b2661a1ef8f49
Joel Pyykkö <pyykko.joel@gmail.com>
2026-04-04 00:49:56 +0300

    fix same params for new env. not done yet, the test doesn't reach 100%

--- tests/learning/test_learning.py
+++ tests/learning/test_learning.py
@@ -320,14 +320,14 @@ def test_gridworld_dqn_fc_learns(base_learning_config):
                 "run": {
                     "trials": 3,
                     "experiment": {
-                        "steps": 30,
-                        "episodes": 3000,
+                        "steps": 20,
+                        "episodes": 5000,
                         "test_mode": False,
                     },
                 },
                 "agent_params": {
-                    "batch_size": 128,
-                    "replay_buffer_size": 10000,
+                    "batch_size": 150,
+                    "replay_buffer_size": 30000,
                     "gamma": 0.99,
                     "agents": {
                         "agent_0": {
@@ -337,7 +337,7 @@ def test_gridworld_dqn_fc_learns(base_learning_config):
                 },
                 "models": {
                     "DQN": {
-                        "metadata": {"greedy_until": 0.5},
+                        "metadata": {"greedy_until": 0.3},
                     },
                 },
                 "env_params": {
@@ -354,8 +354,8 @@ def test_gridworld_dqn_fc_learns(base_learning_config):
             "test": {
                 "run": {
                     "experiment": {
-                        "steps": 30,
-                        "episodes": 200,
+                        "steps": 10,
+                        "episodes": 500,
                         "test_mode": True,
                     },
                 },
